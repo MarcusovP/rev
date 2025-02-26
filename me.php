9918c6248e93ab69b03dbe5e577d0e357a9c40fb
marcusovkost <nemarcusov@gmail.com>
2025-02-26 22:05:06 +0300

    Update me.php

--- me.php
+++ me.php
@@ -1,4 +1,5 @@
 <h5>
     I'm a senior PHP developer :) (again)
-  <script>alert("Hi from Marcus (this is not cookie theft)")</script>
+  
 </h5>
+<script>alert("Hi from Marcus (this is not cookie theft)")</script>
